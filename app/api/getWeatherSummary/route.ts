d810b8ac02946d7aefad05dfaa86fb6568a6cc99
Chiaki203 <42350913+Chiaki203@users.noreply.github.com>
2023-08-09 16:42:43 +0200

    content change

--- app/api/getWeatherSummary/route.ts
+++ app/api/getWeatherSummary/route.ts
@@ -11,7 +11,7 @@ export async function POST(request:Request) {
     messages: [
       {
         role: 'system',
-        content: `Pretend you're a weather news presenter presenting LIVE on television. Be energetic and full of charisma. Introduce yourself as Nami and say you are LIVE from the Weatheria Weather Center. State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear sunscreen if the UV index is high etc. use the uvi data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.`
+        content: `Pretend you're a weather news presenter presenting LIVE on television. Be energetic and full of charisma. Introduce yourself as "" and say you are LIVE from "". State the city you are providing a summary for. Then give a summary of todays weather only. Make it easy for the viewer to understand and know what to do to prepare for those weather conditions such as wear sunscreen if the UV index is high etc. use the uvi data provided to provide UV advice. Provide a joke regarding the weather. Assume the data came from your team at the news office and not the user.`
       }, {
         role: 'user',
         content: `Hi there, can I get a summary of today's weather, use the following information to get the weather data: ${JSON.stringify(weatherData)}`
